0e1bf510e5fe4d8543c5c98a73770e90cedc39d3
Bac0nator0902 <lucas123famous@gmail.com>
2022-11-03 17:10:31 -0400

    styled nav a little

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webmaster2022-2023
 Webmaster2022-2023
+
+TSA Webmaster 2022-2023
+Space theme
---
+++ js/home.js
@@ -0,0 +1,10 @@
+function showNav(id) {
+    container = document.getElementById("hiddenNav-" + id);
+    container.style.display = "block";
+}
+
+
+function  hideNav(id) {
+    container = document.getElementById("hiddenNav-" + id);
+    container.style.display = "none";
+}
