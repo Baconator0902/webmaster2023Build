d531a8d37cc486d2d83289f10d3c0264b0d3e17a
Lucas Famous <lucas.j.famous@gmail.com>
2022-12-18 22:28:26 -0500

    booking and styling

---
+++ js/booking.js
@@ -0,0 +1,28 @@
+
+function showBookFlight() {
+    bookingDiv = document.getElementById("bookTripDIV");
+    trainingDiv = document.getElementById("bookFlightTraining");
+    physicalDiv = document.getElementById("bookAstroPhys");
+    bookingDiv.style.display = "block";
+    trainingDiv.style.display = "none";
+    physicalDiv.style.display = "none";
+}
+
+function showTraining() {
+    bookingDiv = document.getElementById("bookTripDIV");
+    trainingDiv = document.getElementById("bookFlightTraining");
+    physicalDiv = document.getElementById("bookAstroPhys");
+
+    bookingDiv.style.display = "none";
+    trainingDiv.style.display = "block";
+    physicalDiv.style.display = "none";
+}
+
+function showBookPhysical() {
+    bookingDiv = document.getElementById("bookTripDIV");
+    trainingDiv = document.getElementById("bookFlightTraining");
+    physicalDiv = document.getElementById("bookAstroPhys");
+    bookingDiv.style.display = "none";
+    trainingDiv.style.display = "none";
+    physicalDiv.style.display = "block";
+}
