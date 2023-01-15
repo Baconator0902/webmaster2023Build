2ee0db37ce41426d0cdd4ab1bfd6383ab9cc1933
Lucas Famous <lucas.j.famous@gmail.com>
2023-01-14 23:48:15 -0500

    faq, info

--- js/faqs.js
+++ js/faqs.js
@@ -2,13 +2,21 @@ var timesRan = {"FAQ1":0, "FAQ2":0, "FAQ3":0, "FAQ4":0, "FAQ5":0, "FAQ6":0,"FAQ7
 
 function showAnswer(num) {
     divid = 'div' + num;
+    plusId = 'plusImg' + num;
+    minusId = 'minusImg' + num;
     var timesRanVar = "FAQ" + num;
     var x = document.getElementById(divid);
+    var plusImg = document.getElementById(plusId);
+    var minusImg = document.getElementById(minusId);
     if (x.style.display == "none" || timesRan[timesRanVar] == 0) {
         x.style.display = 'block';
+        minusImg.style.display = 'block';
+        plusImg.style.display = 'none';
         x.animate([{ opacity: "0" },{ opacity: "1" }], {duration: 300});
     } else {
         x.style.display = 'none';
+        minusImg.style.display = 'none';
+        plusImg.style.display = 'inline';
     }
     timesRan[timesRanVar]++;
     console.log(timesRan[timesRanVar]);
