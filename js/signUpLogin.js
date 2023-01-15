7e2492d0b5b8c15d3631748097643209e5274c6f
Lucas Famous <lucas.j.famous@gmail.com>
2023-01-14 22:38:42 -0500

    stuff

--- js/signUpLogin.js
+++ js/signUpLogin.js
@@ -1,17 +1,24 @@
 function signUpShow() {
-
+    showSignUpButton = document.getElementById("showSignUpButton");
+    showLogInButton = document.getElementById("showLogInButton");
     signUpDiv = document.getElementById("signUpDiv");
     logInDiv = document.getElementById("logInDiv");
 
     signUpDiv.style.display = "block";
     logInDiv.style.display = "none";
+    showSignUpButton.style.display = "none";
+    showLogInButton.style.display = "block";
 }
 
 function logInShow() {
-
+    showSignUpButton = document.getElementById("showSignUpButton");
+    showLogInButton = document.getElementById("showLogInButton");
     signUpDiv = document.getElementById("signUpDiv");
     logInDiv = document.getElementById("logInDiv");
 
     signUpDiv.style.display = "none";
     logInDiv.style.display = "block";
+    showSignUpButton.style.display = "block";
+    showLogInButton.style.display = "none";
 }
+
